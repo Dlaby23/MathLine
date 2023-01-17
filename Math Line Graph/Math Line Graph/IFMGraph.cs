6c2111cc98579eba78dda08df85687829e5a0a4c
Dlaby23 <107836719+Dlaby23@users.noreply.github.com>
2023-01-17 11:30:55 +0100

    Add files via upload

---
+++ Math Line Graph/Math Line Graph/IFMGraph.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+
+namespace Math_Line_Graph
+{
+    public class IFMGraph
+    {
+        public const int Width = 221;
+        public const int Height = 84;
+        public string[,] _soustava = new string[Height, Width];
+        public void Run()
+        {            
+            VytvářeníGraph();
+            Console.Write("pro k: ");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("pro q: ");
+            double q = Convert.ToDouble(Console.ReadLine());
+            PočítáníRovnice(-k, -q);
+            KresleníGraph();     
+
+        }
+        public void VytvářeníGraph()
+        {
+            
+            for (var i = 0; i < Height; i++)
+            {
+                for (var j = 0; j < Width; j++)
+                {
+                    if (j == (Width / 2))
+                    {
+                        _soustava[i, j] = "|";
+                    }
+                    else if (i == (Height / 2))
+                    {
+                        _soustava[i, j] = "-";
+                    }
+                    else
+                    {
+                        _soustava[i, j] = " ";
+                    }
+                }
+            }
+        }
+
+        public void KresleníGraph()
+        {
+            for (var i = 0; i < Height; i++)
+            {
+                for (var j = 0; j < Width; j++)
+                {
+                    Console.Write(_soustava[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void ZapsáníGraphu(double x, double y)
+        {
+            try
+            {
+                //Pragram na každou sořadnici x a y zapíše "*"
+                _soustava[(int)y, (int)x] = "*";
+            }
+            catch
+            {
+                //program nic neudělá nic a běží dál
+            }
+
+        }
+
+        public void PočítáníRovnice(double k, double q)
+        {
+            //upravuje délku přímky
+            for (double x = -100.0; x < 100.0; x++)
+            {
+                double y = k * x + q;
+                ZapsáníGraphu(x + (Width / 2.0), y + (Height / 2.0));
+
+            }            
+        }        
+    }
+}
---
+++ Math Line Graph/Math Line Graph/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Math_Line_Graph
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new IFMGraph();
+            while (true)
+            {
+
+                app.Run();
+                Console.WriteLine("chcete porkačovat? Ano/Ne/Clear");
+                string a = Console.ReadLine();
+                if (a == "Ano")
+                {
+                    continue;
+                }
+                else if (a == "Clear")
+                {
+                    Console.Clear();
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
